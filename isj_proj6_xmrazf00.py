83e892d87c32caff1e3e6de7059a680f10ced2e8
Diktator6 <filip-mraz@seznam.cz>
2022-04-27 21:10:02 +0200

    Proj 6 pre

---
+++ isj_proj6_xmrazf00.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class Polynomial():
+
+    def __init__(self, *args, **kwargs):
+        self.rad =
+        pass
+
+    def __str__(self):
+        pass
+
+    def __eq__(self):
+        pass
+
+    def __add__(self, other):
+        pass
+
